38962b7ee4b5ead946f8ed302dbc2b0ecffdb0e2
Arvydas Kavaliauskas <arvydas.kavaliauskas83@gmail.com>
2021-08-07 17:02:19 +0300

    Doctor's patients method  added

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AppointmentController;
 use App\Http\Controllers\DoctorController;
 use App\Http\Controllers\PatientController;
+use App\Http\Controllers\PrescriptionController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -26,9 +27,10 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::resource('doctors', DoctorController::class);
+Route::get('/doctors/{id}/register/', [AppointmentController::class, 'create'])->name('doctor.register-patient');
+Route::get('/doctors/{id}/patients/', [DoctorController::class, 'patients'])->name('doctor.patients');
+Route::resource('/doctors/{doctorId}/patients/{patientId}/prescriptions', PrescriptionController::class);
+Route::resource('appointments', AppointmentController::class);
 Route::get('/patients/', [PatientController::class, 'index'])->name('patients.index');
 Route::get('/patients/create', [PatientController::class, 'create'])->name('patients.create');
 Route::post('/patients/store', [PatientController::class, 'store'])->name('patients.store');
-Route::resource('appointments', AppointmentController::class);
-Route::get('/doctors/{id}/register/', [AppointmentController::class, 'create'])->name('doctor.register-patient');
-Route::resource('/doctors/{doctorId}/patients/{patientId}/prescriptions', PrescriptionController::class);
