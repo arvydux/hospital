c2aaa267d0d7353e80aa294c3971d7fd1ed5cf7f
Arvydas Kavaliauskas <arvydas.kavaliauskas83@gmail.com>
2021-08-11 08:00:43 +0300

    Added seeders for User/Role/User_Roles tables

---
+++ database/seeders/UsersRolesSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\UserRole;
+use Illuminate\Database\Seeder;
+
+class UsersRolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        UserRole::create([
+            'user_id' => '1',
+            'role_id' => '1',
+        ]);
+
+        UserRole::create([
+            'user_id' => '2',
+            'role_id' => '2',
+        ]);
+
+        UserRole::create([
+            'user_id' => '3',
+            'role_id' => '3',
+        ]);
+
+        UserRole::create([
+            'user_id' => '4',
+            'role_id' => '4',
+        ]);
+    }
+}
