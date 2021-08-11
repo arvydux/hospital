a3729c3b59506359b35ad915bcf0d1ae8624cc8e
Arvydas Kavaliauskas <arvydas.kavaliauskas83@gmail.com>
2021-08-11 09:09:49 +0300

    Removed permission code

--- app/Models/User.php
+++ app/Models/User.php
@@ -65,49 +65,4 @@ class User extends Authenticatable
         return false;
     }
 
-    public function hasPermission($permission)
-    {
-        return $this->hasPermissionThroughRole($permission) || (bool) $this->permissions->where('name',$permission->name)->count();
-    }
-
-    public function hasPermissionThroughRole($permission)
-    {
-        foreach($permission->roles as $role)
-        {
-            if($this->roles->contains($role))
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public function givePermission(...$permission)
-    {
-        $permissions = $this->getPermissions(array_flatten($permission));
-        if($permissions === null)
-        {
-            return $this;
-        }
-        $this->permissions()->saveMany($permissions);
-        return $this;
-    }
-
-    public function getPermissions(array $permissions)
-    {
-        return Permission::whereIn('name',$permissions)->get();
-    }
-
-    public function removePermission(...$permission)
-    {
-        $permissions = $this->getPermissions(array_flatten($permission));
-        $this->permissions()->detach($permissions);
-        return $this;
-    }
-
-    public function modifyPermission(...$permissions)
-    {
-        $this->permissions()->detach();
-        return $this->givePermission($permissions);
-    }
 }
